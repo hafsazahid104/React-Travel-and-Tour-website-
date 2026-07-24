73f20bad45f3dc2ce9362947f33f7dbaba15e048
Hafsazahid104 <hafsazahid955@gmail.com>
2026-07-25 02:15:42 +0500

    fix import case

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom'
-import Header from './Pages/header'
-import Footer from './Pages/Footer'
+import Header from './Pages/Header.jsx'
+import Footer from './Pages/Footer.jsx'
 
 function Layout(){
   return(
